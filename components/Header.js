1bec195ac5a82055cdbda52b40cebe18bad867e1
yekhaung <yekhaung.dev@gmail.com>
2018-11-24 22:52:16 +0800

    Init project. Setup pj structure and skeleton codes.

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Shop Demo</h1>
+      <Nav />
+    </div>
+  );
+};
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <p>Hello!!!</p>
+    </div>
+  );
+};
+
+export default Index;
