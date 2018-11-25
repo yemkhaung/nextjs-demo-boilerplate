78ea4c880be8c97649e5efa98c9c5626b8c4ee39
yekhaung <yekhaung.dev@gmail.com>
2018-11-25 22:03:38 +0800

    Improved readme. minimal styling

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# nextjs-demo-shop-app
-Demo shop application NextJS (Server-rendered ReactJS)
+# NextJS Demo (Shop App)
+
+A *Shop Application* to demonstrate *server-side rendered* ReactJS application using Next.js.
+
+Library being used here is [Next.js](https://nextjs.org/) which supports nice features such as...
+
+* Server side rendering
+* File system routing
+* Automatic code-splitting, etc.
+
+## Getting Started
+
+### Run Application
+```bash
+npm run dev
+```
+Afterwards, access http://localhost:3000
+
+### Production
+```bash
+npm run build
+npm run start
+```
--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,52 @@
-import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+
 import Nav from "./Nav";
+
+const StyledHeader = styled.div`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+  }
+  .sub-bar {
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
+  }
+`;
+
+const Logo = styled.h1`
+  transform: skew(-10deg);
+  position: relative;
+  z-index: 2;
+  a {
+    background: ${props => props.theme.red};
+    font-size: 4rem;
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+    margin-left: 2rem;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
 
 const Header = () => {
   return (
-    <div>
-      <h1>Shop Demo</h1>
-      <Nav />
-    </div>
+    <StyledHeader>
+      <div className="bar">
+        <Logo>
+          <Link href="/">
+            <a>Shop Demo</a>
+          </Link>
+        </Logo>
+        <Nav />
+      </div>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
+      <div>Cart</div>
+    </StyledHeader>
   );
 };
 
